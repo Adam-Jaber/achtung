c5ea5b10b3891340df70f4691c925e19fdc1b433
adam-jaber <adam.jaber248@gmail.com>
2022-02-21 15:28:24 +0200

    create pygame screen with player that moves freely using arrow buttons

---
+++ client.py
@@ -0,0 +1,57 @@
+import pygame
+import random
+import _thread
+from player import Player
+
+#get this dicts, player_list and your player from server
+reverse_dict = {(0,255,0): False}
+start_pos_dict = {(0,255,0):(200,200)}
+angle_dict = {(0,255,0): 50}
+players_list = [(0,255,0)]
+my_player = (0,255,0)
+
+
+SCREEN_SIZE = (800, 600)
+GAME_SIZE = (600,600)
+PLAYER_VEL = 5
+
+
+players_dict = dict()
+for player_color in players_list:
+    players_dict[player_color] = Player(start_pos_dict[player_color])
+
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+
+def check_rotation(player_color):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        angle_dict[player_color] = -0.3
+        print('left')
+    elif keys[pygame.K_RIGHT]:
+        angle_dict[player_color] = 0.3
+        print('right')
+    else:
+        angle_dict[player_color] = 0
+
+
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((200,200,200))
+
+    game_surface = pygame.Surface(GAME_SIZE)
+    game_surface.fill((0,0,0))
+
+    check_rotation(my_player)
+
+    for player_color in players_list:
+        reverse_dict[player_color] = players_dict[player_color].next_pos(angle_dict[player_color], reverse_dict[player_color])
+        pygame.draw.lines(screen, player_color, False, players_dict[player_color].get_pos_list()[1:])
+
+    pygame.display.update()
---
+++ player.py
@@ -0,0 +1,30 @@
+import math
+
+PLAYER_VEL = 0.1
+
+class Player:
+    def __init__(self, first_pos):
+        self.pos_list = [(0,0),first_pos]
+
+    def next_pos(self, ang, reverse):
+        slope = (self.pos_list[-1][1] - self.pos_list[-2][1]) / (self.pos_list[-1][0] - self.pos_list[-2][0])
+        if slope > 0:
+            new_slope_ang = ang + math.degrees(math.atan(slope))
+        else:
+            new_slope_ang = ang + (math.degrees(math.atan(slope)) + 180)
+        new_slope = math.tan(math.radians(new_slope_ang))
+        if (ang > 0 and slope > 0 and new_slope < 0) or (ang < 0 and slope < 0 and new_slope > 0):
+            reverse = not reverse
+        if reverse:
+            x_change = - PLAYER_VEL / math.sqrt(1 + math.pow(new_slope, 2))
+        else:
+            x_change = PLAYER_VEL / math.sqrt(1 + math.pow(new_slope, 2))
+        new_x = self.pos_list[-1][0] + x_change
+        new_y = self.pos_list[-1][1] + new_slope * x_change
+        new_pos = (new_x, new_y)
+        self.pos_list.append(new_pos)
+        return reverse
+
+    def get_pos_list(self):
+        return  self.pos_list
+
